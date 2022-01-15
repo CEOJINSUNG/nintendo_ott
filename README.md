c3558d0b02f1ef82ad18b79f246fd0f3c2e60471
CEOJINSUNG <jinsung1048@gmail.com>
2022-01-15 17:44:36 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# nintendo_ott
+ 멀티 모듈을 활용한 Spring Nintendo Ott 서버입니다. 
---
+++ src/main/java/com/multi/module/nintendo_ott/NintendoOttApplication.java
@@ -0,0 +1,13 @@
+package com.multi.module.nintendo_ott;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NintendoOttApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NintendoOttApplication.class, args);
+	}
+
+}
