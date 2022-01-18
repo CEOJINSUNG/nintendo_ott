1c21908eb787b386753b37405ea601f275f0a7f3
CEOJINSUNG <jinsung1048@gmail.com>
2022-01-18 16:30:59 +0900

    Create PartyRepository.java

---
+++ core/src/main/java/com/multi/module/repository/PartyRepository.java
@@ -0,0 +1,9 @@
+package com.multi.module.repository;
+
+import com.multi.module.domain.Party;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PartyRepository extends JpaRepository<Party, Long> {
+}
