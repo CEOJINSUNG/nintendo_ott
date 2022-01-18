db4c7dad32abefb4a29d65a578c2e00678646325
CEOJINSUNG <jinsung1048@gmail.com>
2022-01-18 14:21:28 +0900

    Update User.java

--- core/src/main/java/com/multi/module/domain/User.java
+++ core/src/main/java/com/multi/module/domain/User.java
@@ -1,4 +1,30 @@
 package com.multi.module.domain;
 
-public class User {
+import lombok.Getter;
+import lombok.NonNull;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.*;
+
+@Entity(name = "user")
+@Getter
+@Setter
+@ToString
+public class User extends BaseTime {
+    @Id @Column(name = "user_id", nullable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NonNull
+    private String name;
+    @NonNull
+    private String email;
+    private String profileImage;
+    private String nintendoId;
+
+    @Enumerated(EnumType.STRING)
+    private Bank bank; // 은행 종류 출력
+    private String account;
+
 }
