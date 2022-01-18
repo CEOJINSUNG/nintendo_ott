f4095021c745ca2b3ed6c502782137075f87aafa
CEOJINSUNG <jinsung1048@gmail.com>
2022-01-18 15:07:49 +0900

    Create UserParty.java

---
+++ core/src/main/java/com/multi/module/domain/UserParty.java
@@ -0,0 +1,40 @@
+package com.multi.module.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity(name = "user_party")
+@Getter
+@Setter
+public class UserParty extends BaseTime {
+    @Id @GeneratedValue
+    @Column(name = "user_party_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "party_id")
+    private Party party;
+
+    @Enumerated(EnumType.STRING)
+    private Role role; // LEADER, PARTICIPANT
+
+    private int price;
+    private String description;
+
+    //연관관계 메서드
+    public void setUser(User user) {
+        this.user = user;
+        user.getUserParties().add(this);
+    }
+
+    public void setParty(Party party) {
+        this.party = party;
+        party.getUserParties().add(this);
+    }
+}
