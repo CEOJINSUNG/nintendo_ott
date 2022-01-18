cc3affa004000770b9ec7db8e5ac32b333ef27b2
CEOJINSUNG <jinsung1048@gmail.com>
2022-01-18 15:07:36 +0900

    Update Party.java

--- core/src/main/java/com/multi/module/domain/Party.java
+++ core/src/main/java/com/multi/module/domain/Party.java
@@ -1,4 +1,29 @@
 package com.multi.module.domain;
 
-public class Party {
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity(name = "party")
+@Getter
+@Setter
+@ToString
+public class Party extends BaseTime {
+
+    @Id @Column(name = "party_id", nullable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToMany(mappedBy = "party")
+    private List<UserParty> userParties = new ArrayList<>();
+
+    private String title;
+    private LocalDateTime startDate;
+    private LocalDateTime endDate;
+
 }
