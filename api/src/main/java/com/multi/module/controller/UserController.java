aafa521d8a95f7f0967af1ced3ad23c9ffd70e6a
CEOJINSUNG <jinsung1048@gmail.com>
2022-01-23 22:22:43 +0900

    Update UserController.java

--- api/src/main/java/com/multi/module/controller/UserController.java
+++ api/src/main/java/com/multi/module/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.multi.module.controller;
 
+import com.multi.module.domain.User;
 import com.multi.module.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,10 +10,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 public class UserController {
+
     private final UserService userService;
 
     @GetMapping("/oauth2/authorization/kakao")
-    public void oauth2AuthorizationKakao(@RequestParam("code") String code) {
-        userService.oauth2AuthorizationKakao(code);
+    public User loginByKakao(@RequestParam("code") String code) {
+        return userService.oauth2AuthorizationKakao(code);
     }
 }
