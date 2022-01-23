11ff7c9ca866abaa14bf3a0c05edb914d70c16f7
CEOJINSUNG <jinsung1048@gmail.com>
2022-01-23 23:33:52 +0900

    Update ApiApplication.java

--- api/src/main/java/com/multi/module/ApiApplication.java
+++ api/src/main/java/com/multi/module/ApiApplication.java
@@ -1,11 +1,18 @@
 package com.multi.module;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@EnableJpaAuditing
 @SpringBootApplication
+@EntityScan(basePackages = {"com.multi.module.domain"})
+@EnableJpaAuditing
+@EnableJpaRepositories(basePackages = {"com.multi.module.repository"})
 public class ApiApplication {
     public static void main(String[] args) {
         SpringApplication.run(ApiApplication.class, args);
