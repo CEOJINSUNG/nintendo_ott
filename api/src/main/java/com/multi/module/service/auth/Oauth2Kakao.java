16d6a61b34752948d46c9aad4d6870c1991589de
CEOJINSUNG <jinsung1048@gmail.com>
2022-01-25 13:44:17 +0900

    Update Oauth2Kakao.java

--- api/src/main/java/com/multi/module/service/auth/Oauth2Kakao.java
+++ api/src/main/java/com/multi/module/service/auth/Oauth2Kakao.java
@@ -25,7 +25,8 @@ public class Oauth2Kakao {
     private final ObjectMapper objectMapper;
 
     private final String kakaoOauth2ClientId = "8aac3bd7d302e49db68923a6ebc79048";
-    private final String frontendRedirectUrl = "http//localhost:3000";
+    private final String kakaoOauth2ClientSecret = "ZLCIgJ177NEkFYNbsH7UjNVhIRa8kJMA";
+    private final String frontendRedirectUrl = "http://localhost:3000/login";
 
     public AuthorizationKakao callTokenApi(String code) {
         String grantType = "authorization_code";
@@ -36,7 +37,8 @@ public class Oauth2Kakao {
         MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
         params.add("grant_type", grantType);
         params.add("client_id", kakaoOauth2ClientId);
-        params.add("redirect_uri", frontendRedirectUrl + "/callback/kakao");
+        params.add("client_secret", kakaoOauth2ClientSecret);
+        params.add("redirect_uri", frontendRedirectUrl);
         params.add("code", code);
 
         HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<>(params, headers);
@@ -44,6 +46,7 @@ public class Oauth2Kakao {
         String url = "https://kauth.kakao.com/oauth/token";
         try {
             ResponseEntity<String> response = restTemplate.postForEntity(url, request, String.class);
+            System.out.println(response);
             AuthorizationKakao authorization = objectMapper.readValue(response.getBody(), AuthorizationKakao.class);
             return authorization;
         } catch (JsonMappingException e) {
@@ -59,6 +62,7 @@ public class Oauth2Kakao {
     public String callGetUserByAccessToken(String accessToken) {
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+        headers.set("Authorization", "Bearer "+accessToken);
 
         MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
         HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<>(params, headers);
