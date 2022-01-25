3d4fb5acc79a9151891abca41a0377047b5ff0a3
CEOJINSUNG <jinsung1048@gmail.com>
2022-01-25 13:43:59 +0900

    Update EtcConfig.java

--- api/src/main/java/com/multi/module/config/EtcConfig.java
+++ api/src/main/java/com/multi/module/config/EtcConfig.java
@@ -2,12 +2,26 @@ package com.multi.module.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.client.ClientHttpRequest;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.web.client.DefaultResponseErrorHandler;
 import org.springframework.web.client.RestTemplate;
+
+import java.io.IOException;
 
 @Configuration
 public class EtcConfig {
     @Bean
     public RestTemplate restTemplate() {
-        return new RestTemplate();
+        RestTemplate restTemplate = new RestTemplate();
+        restTemplate.setRequestFactory(new HttpComponentsClientHttpRequestFactory());
+        restTemplate.setErrorHandler(new DefaultResponseErrorHandler() {
+            public boolean hasError(ClientHttpRequest clientHttpRequest) throws IOException {
+                HttpStatus status = clientHttpRequest.execute().getStatusCode();
+                return status.series() == HttpStatus.Series.SERVER_ERROR;
+            }
+        });
+        return restTemplate;
     }
 }
