90ed96bcab2d52dbbe9524e598aed8b7844ee258
CEOJINSUNG <jinsung1048@gmail.com>
2022-01-25 13:43:56 +0900

    Update ApiApplication.java

--- api/src/main/java/com/multi/module/ApiApplication.java
+++ api/src/main/java/com/multi/module/ApiApplication.java
@@ -1,11 +1,8 @@
 package com.multi.module;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
