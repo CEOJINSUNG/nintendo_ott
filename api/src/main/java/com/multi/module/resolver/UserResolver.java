afb0c6eb35a1de864e53fddfb294443fa056c17f
CEOJINSUNG <jinsung1048@gmail.com>
2022-01-25 13:44:12 +0900

    Update UserResolver.java

--- api/src/main/java/com/multi/module/resolver/UserResolver.java
+++ api/src/main/java/com/multi/module/resolver/UserResolver.java
@@ -12,19 +12,23 @@ public class UserResolver implements GraphQLResolver<User> {
 
     private final UserRepository userRepository;
 
-    public User getUser(int id) {
+    public User getUser(Long id) {
         return userRepository.findById(id).orElseThrow(null);
     }
 
+    public Long getUser_id(User user) {
+        return userRepository.findUserByKakaoId(user.getKakaoId()).get().getId();
+    }
+
     public String getName(User user) {
-        return userRepository.findById(user.getId()).get().getName();
+        return userRepository.findUserByKakaoId(user.getKakaoId()).get().getName();
     }
 
     public String getProfile_image(User user) {
-        return userRepository.findById(user.getId()).get().getProfileImage();
+        return userRepository.findUserByKakaoId(user.getKakaoId()).get().getProfileImage();
     }
 
     public String getNintendo_id(User user) {
-        return userRepository.findById(user.getId()).get().getNintendoId();
+        return userRepository.findUserByKakaoId(user.getKakaoId()).get().getNintendoId();
     }
 }
