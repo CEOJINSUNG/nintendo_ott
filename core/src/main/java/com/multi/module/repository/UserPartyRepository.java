763d4b5eb355f9de13204acb12346dcea3cb2577
CEOJINSUNG <jinsung1048@gmail.com>
2022-02-04 16:40:49 +0900

    Update UserPartyRepository.java

--- core/src/main/java/com/multi/module/repository/UserPartyRepository.java
+++ core/src/main/java/com/multi/module/repository/UserPartyRepository.java
@@ -1,9 +1,15 @@
 package com.multi.module.repository;
 
+import com.multi.module.domain.Party;
+import com.multi.module.domain.User;
 import com.multi.module.domain.UserParty;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
+
 @Repository
 public interface UserPartyRepository extends JpaRepository<UserParty, Long> {
+    UserParty findUserPartiesByUser(User user);
+    ArrayList<UserParty> findUserPartiesByParty(Party party);
 }
